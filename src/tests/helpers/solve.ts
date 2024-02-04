932075e4d88ee35d9437e5517911d26422c4c789
Herb Caudill <herb@devresults.com>
2024-02-04 13:01:42 +0100

    helpers/solve.ts:

--- src/tests/helpers/solve.ts
+++ src/tests/helpers/solve.ts
@@ -3,7 +3,7 @@ import { printGrid } from '../../printGrid.js'
 import { Puzzle } from '../../Puzzle.js'
 
 export const solve = (puzzle: string, maxSteps: number = 10000) => {
-  const { solved, error } = new Puzzle(puzzle, { maxSteps }).analyze(maxSteps)
+  const { solved, error } = new Puzzle(puzzle).analyze(maxSteps)
   if (!solved) {
     const grid = toGrid(puzzle)
     throw new Error(`${error}:\n${printGrid(grid)}`)
