81f6903210318834a96531eb8e16dac8802de48a
Herb Caudill <herb@devresults.com>
2024-02-18 19:13:30 +0100

    touch-none

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -33,7 +33,7 @@ export const App = () => {
   return puzzle && solution ? (
     <>
       <div
-        className="h-dvh p-2 pb-12 w-[36rem] max-w-full mx-auto flex flex-col gap-4 select-none relative"
+        className="h-dvh p-2 pb-12 w-[36rem] max-w-full mx-auto flex flex-col gap-4 touch-none select-none relative"
         style={{ containerType: 'size' }}
       >
         <div className="grow">
