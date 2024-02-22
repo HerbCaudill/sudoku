983ada5fe69db58a3e6d26e1ce10e5f018d2131e
Herb Caudill <herb@devresults.com>
2024-02-22 22:35:01 +0100

    move reducer out of HumanSolver

--- src/components/HumanSolver.tsx
+++ src/components/HumanSolver.tsx
@@ -1,98 +1,17 @@
 import { useEffect, useReducer, useState } from 'react'
 import { useHotkeys } from 'react-hotkeys-hook'
-import { CandidateGrid, Grid, numbers } from '../solver'
+import { Grid, numbers } from '../solver'
 import { Puzzle } from './Puzzle'
 import { RadioGroup } from './RadioGroup'
 import { Confetti } from './Confetti'
+import { reducer, getInitialState } from '../reducer'
 
 const numberKeys = numbers.map(n => n.toString())
 
 export const HumanSolver = ({ puzzle, solution, onNewGame }: Props) => {
   // STATE
 
-  const reducer = (state: State, action: Action): State => {
-    const updateState = (newState: Partial<State>) => {
-      return {
-        ...state,
-        ...newState,
-        history: [state, ...state.history], // add the previous state to the history
-        future: [], // new actions reset the redo stack
-      }
-    }
-
-    switch (action.type) {
-      // STATE UPDATES
-
-      case 'SET': {
-        const { index: i, value } = action
-
-        // set value
-        const grid = [...state.grid]
-        grid[i] = value
-
-        // remove candidates
-        const candidates = { ...state.candidates }
-        delete candidates[i]
-
-        return updateState({ grid, candidates, index: i })
-      }
-
-      case 'ADD': {
-        const { index, candidate } = action
-        const candidates = { ...state.candidates }
-        const cell = candidates[index] ?? []
-        if (cell.includes(candidate)) return state // don't add if already present
-
-        // add candidate
-        candidates[index] = [...cell, candidate]
-
-        return updateState({ candidates, index: 0 })
-      }
-
-      case 'REMOVE': {
-        const { index, candidate } = action
-        const candidates = { ...state.candidates }
-        const cell = candidates[index] ?? []
-        if (!cell.includes(candidate)) return state // don't remove if not present
-
-        // remove candidate
-        candidates[index] = cell.filter(n => n !== candidate)
-
-        return updateState({ candidates, index: 0 })
-      }
-
-      case 'RESET': {
-        return updateState({ grid: puzzle, candidates: {}, history: [], future: [] })
-      }
-
-      // UNDO/REDO
-
-      case 'UNDO': {
-        if (!state.history.length) return state
-
-        const [prevState, ...history] = state.history
-        const future = [state, ...state.future]
-        return { ...prevState, history, future }
-      }
-
-      case 'REDO': {
-        if (!state.future.length) return state
-
-        const [nextState, ...future] = state.future
-        const history = [state, ...state.history]
-        return { ...nextState, history, future }
-      }
-    }
-  }
-
-  const initialState = {
-    grid: puzzle,
-    candidates: {},
-    index: -1,
-    history: [],
-    future: [],
-  }
-  const [state, dispatch] = useReducer(reducer, initialState)
+  const [state, dispatch] = useReducer(reducer, getInitialState(puzzle))
 
   useEffect(() => {
     reset()
@@ -227,22 +146,6 @@ export const HumanSolver = ({ puzzle, solution, onNewGame }: Props) => {
       )}
     </div>
   )
-}
-
-type Action =
-  | { type: 'SET'; index: number; value: number }
-  | { type: 'ADD'; index: number; candidate: number }
-  | { type: 'REMOVE'; index: number; candidate: number }
-  | { type: 'RESET' }
-  | { type: 'UNDO' }
-  | { type: 'REDO' }
-
-type State = {
-  grid: Grid
-  candidates: CandidateGrid
-  index: number
-  history: State[]
-  future: State[]
 }
 
 type Props = {
