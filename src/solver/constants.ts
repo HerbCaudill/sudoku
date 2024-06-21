4b4d5bf0921749b4ac9b84821c6d32056c49d2db
Herb Caudill <herb@devresults.com>
2024-06-21 16:04:05 +0200

    constants:

--- src/solver/constants.ts
+++ src/solver/constants.ts
@@ -53,4 +53,5 @@ export const emptyGrid: Grid = toGrid(`
 
 export const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-export const cells = Object.keys(emptyGrid).map(Number)
+// numbers from 0 to 80
+export const cells = Array.from({ length: 81 }, (_, i) => i)
